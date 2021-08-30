b2103aabc8226c2ba92192b8121ce8fc650f60a2
Mohamed Yousef <engineer.mohamed.yossef@gmail.com>
2021-08-30 14:05:56 +0200

    working on game screen

--- main.js
+++ main.js
@@ -22,19 +22,36 @@ document.querySelectorAll('.home ul li').forEach(function(item){
     });
 });
 
-document.querySelectorAll('.menu-button').forEach(function (item) {
-    item.addEventListener("click", function () {
+document.querySelectorAll('.menu-button').forEach(function(item) {
+    item.addEventListener("click", function() {   
         document.getElementById('home-buttons').classList.add('hidden');
         document.getElementById(this.getAttribute('data-screen')).classList.remove('hidden');
+
+        // hide header / footer , in case the game screen
+        if (this.getAttribute('data-screen') == 'game') {
+            document.querySelector('.home h1').classList.add('hidden');
+            document.querySelector('.home small').classList.add('hidden');
+            document.querySelector('footer').classList.add('hidden');
+        }
     });
 });
 
-document.querySelectorAll('.back-button').forEach(function (item) {
-    item.addEventListener("click", function () {
+document.querySelectorAll('.back-button').forEach(function(item) {
+    item.addEventListener("click", function() {
         document.querySelectorAll('.screen').forEach(function(item){
             item.classList.add('hidden');
         });
 
         document.getElementById('home-buttons').classList.remove('hidden');
     });
+});
+
+document.querySelector('#game-back-button').addEventListener("click", function() {
+    if (!confirm('Are You Sure?')) return;
+
+    document.getElementById('game').classList.add('hidden');
+    document.getElementById('home-buttons').classList.remove('hidden');
+    document.querySelector('.home h1').classList.remove('hidden');
+    document.querySelector('.home small').classList.remove('hidden');
+    document.querySelector('footer').classList.remove('hidden');
 });
